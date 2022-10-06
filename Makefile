f32bfac4187c72f56e91f965b3791440bc3f2bcf
Samuel <ottonsam@gmail.com>
2022-10-05 21:15:12 -0300

    scripts uteis

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 run:
 	g++ -Wall auxiliar.cpp -o auxiliar
 	g++ -Wall sequencial.cpp -o sequencial
-	g++ -Wall threads.cpp -o threads
+	g++ -Wall threads.cpp -o threads -pthread
 	g++ -Wall processos.cpp -o processos
---
+++ sequencial.sh
@@ -0,0 +1,5 @@
+./sequencial matrizes/matriz1-100X100.txt matrizes/matriz2-100X100.txt
+./sequencial matrizes/matriz1-200X200.txt matrizes/matriz2-200X200.txt
+./sequencial matrizes/matriz1-400X400.txt matrizes/matriz2-400X400.txt
+./sequencial matrizes/matriz1-800X800.txt matrizes/matriz2-800X800.txt
+./sequencial matrizes/matriz1-1600X1600.txt matrizes/matriz2-1600X1600.txt
